e63fa5658d5f90cc88c772f12747c57b869a6dba
KinDR007 <61905170+KinDR007@users.noreply.github.com>
2020-06-19 13:17:58 +0200

    Delete config.h

--- Victron2MQTTv1.1/Victron2MQTTv1/config.h
+++
@@ -1,9 +0,0 @@
-const char* ssid = "Automatizace";//Automatizace
-const char* password = "B39b4Ca113F271dB287541BC_K7L";  //password to wifi hidden_password_to_my_wifi
-const char* mqtt_server = "192.168.88.201";             //ip to mqtt server
-const char* mqtt_user = "";                           //mqtt user name
-const char* mqtt_pass = "";                           //mqtt password
-
-#define OTA_HOSTNAME                    "VictronMPPT"
-#define EPSOLAR_DEVICE_ID    1
-#define MQTT_ROOT                       "Victron"
