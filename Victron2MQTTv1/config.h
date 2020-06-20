b9efceb2461dd09c3c7d000b5eaf12484919e7ac
KinDR007 <61905170+KinDR007@users.noreply.github.com>
2020-06-20 08:09:26 +0200

    Update config.h

--- Victron2MQTTv1/config.h
+++ Victron2MQTTv1/config.h
@@ -1,6 +1,6 @@
 const char* ssid = "SSID";
 const char* password = "hidden_password_to_my_wifi";  //password to wifi
-const char* mqtt_server = "192:168.0.10";             //ip to mqtt server
+const char* mqtt_server = "192.168.0.10";             //ip to mqtt server
 const char* mqtt_user = "";                           //mqtt user name
 const char* mqtt_pass = "";                           //mqtt password
 
